1670477ae0ee6eb580f204d6ffc41766a2057827
Raphael-Soares <soaresphaell@gmail.com>
2023-03-10 17:53:07 -0300

    header styled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,65 @@
 import React from "react";
-
+import header from "../assets/header.png";
 import styled from "styled-components";
 
 const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
     background-color: #f5f1edff;
     height: 80vh;
+    padding: 0 5vw;
+`;
+
+const Content = styled.div`
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Media = styled.img`
+    width: 40%;
+`;
+
+const Title = styled.h1`
+    font-size: 2.5rem;
+    font-weight: 700;
+    color: #2d2d2dff;
+`;
+
+const Subtitle = styled.h2`
+    font-size: 1.5rem;
+    font-weight: 500;
+    color: #2d2d2dff;
+`;
+
+const TextGradient = styled.span`
+    background: linear-gradient(120deg, #1192ce 0%, #0018f2 50%, #a600ff 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
 `;
 
 function Header() {
-    return <Container>Header</Container>;
+    return (
+        <Container>
+            <Content>
+                <Title>
+                    Alavanque o seu negócio com
+                    <TextGradient> Landing Pages Profissionais</TextGradient>!
+                </Title>
+                <Subtitle>
+                    Nós criamos Landing Pages personalizadas e profissionais para pequenos negócios
+                    que buscam aumentar sua presença online e converter visitantes em clientes.
+                    Aumente suas vendas e expanda seus negócios conosco!
+                </Subtitle>
+            </Content>
+            <Media src={header} />
+        </Container>
+    );
 }
 
 export default Header;
